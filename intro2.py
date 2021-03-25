9aba2a613f6d1020679385f39f7ea39d091ea4f4
jihanMaulana <80014077+jihanMaulana@users.noreply.github.com>
2021-03-25 10:28:57 +0700

    Create intro2.py

---
+++ intro2.py
@@ -0,0 +1,30 @@
+import sys
+import os
+import time
+import socket
+import random
+
+
+print('     ___ _ __ ______ _ _      ')
+time.sleep(1)
+print('    / ___   /    |  _  \     ')
+time.sleep(3)
+print('   |/   /  / __  | |_)  |    ')
+time.sleep(3)
+print('       /  / /__\ |  ___/     ')
+time.sleep(3)
+print('      /  /       | |         ')
+time.sleep(3)
+print('     /  / __/ \__| |         ')
+time.sleep(3)
+print('                         ')
+print('\n')
+time.sleep(5)
+print('===============================')
+time.sleep(3)
+print('=          doni projek        =')
+time.sleep(3)
+print('===============================')
+time.sleep(3)
+print('AUTHOR : Jihan Maulana')
+print('\n')
